32650989bb00db36350c70697a31806d7d3340ae
laveen-2001 <56155417+laveen-2001@users.noreply.github.com>
2022-03-22 21:43:02 +0530

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -1,7 +1,7 @@
 const key = "18b7aead9bfa5f8a4f6a5f900da2b118";
 
 const getForecast = async (city) => {
-    const base = "http://api.openweathermap.org/data/2.5/forecast";
+    const base = "https://api.openweathermap.org/data/2.5/forecast";
     const query = `?q=${city}&units=metric&appid=${key}`;
     
     const response = await fetch(base+query);
